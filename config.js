0128b56be8914cff0c754b1af4dba1d44c31363e
Rosty <rostyslavvolytskyi@gmqil.com>
2017-03-06 21:53:17 +0000

    local mongoDB successfully setup

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 module.exports = {
 
-  "database" : "mongodb://root:abc123@ds161029.mlab.com:61029/eatlikepro",
+  "database" : "mongodb://localhost:27017/eatlikepro",
   "port" : process.env.PORT || 8080,
   "secretKey" : "YourSecretKey"
 
