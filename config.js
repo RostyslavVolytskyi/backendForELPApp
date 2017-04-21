f15bc76412b6515780da4c71aa9d44d4b554dcef
Rostyy <rostyslavvolytskyi@gmail.com>
2017-04-21 22:34:22 +0200

    deleted config file

--- config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    "database": "mongodb://localhost:27017/eatlikepro",
-    "port": process.env.PORT || 9999,
-    "secretKey": "YourSecretKey",
-    
-    "ELPmail": "eatlikeprofessional@gmail.com",
-    "ELPpass": "uaetlikepro",
-    "adminMails": "eatlikeprofessional@gmail.com, vitaliy.chornyy.dev@gmail.com"
-}
